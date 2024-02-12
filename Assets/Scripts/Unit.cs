e313f1721603f88db628bf3712ed84cfec57b6cf
LiveDierepeater <81476895+LiveDierepeater@users.noreply.github.com>
2024-02-12 12:56:48 +0100

    Took a step back. Started with simplified uncomplex Unit.cs

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,6 +1,28 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
-public abstract class Unit : MonoBehaviour
+[RequireComponent(typeof(NavMeshAgent))]
+public class Unit : MonoBehaviour
 {
+    private NavMeshAgent agent;
     
+    public float MaxSpeed;
+    public float MaxAcceleration;
+    public float MaxDeceleration;
+    public float TurnSpeed;
+    public string UnitName;
+    public float SpeedBonusOnRoad;
+    
+    public enum States
+    {
+        Idle = 0,
+        MoveToDestination = 1,
+        Flee = 2
+    }
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 }
