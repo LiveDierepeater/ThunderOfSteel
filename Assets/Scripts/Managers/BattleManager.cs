19a77b6c776a908105c13180b18de5ea0e9d2eda
LiveDierepeater <81476895+LiveDierepeater@users.noreply.github.com>
2024-04-25 11:13:36 +0200

    Update BattleManager.cs
    - Wrote 3 Methods RegisterAttack(), UnregisterAttack() and UnregisterByKill() to implement BattleManager.cs

--- Assets/Scripts/Managers/BattleManager.cs
+++ Assets/Scripts/Managers/BattleManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
@@ -8,6 +9,8 @@ public class BattleManager : MonoBehaviour
     public event Action<Weaponry, UnitHealth> OnAttackStarted;
     public event Action<Weaponry, UnitHealth> OnAttackStopped;
     public event Action<UnitHealth> OnUnitDied;
+    
+    private Dictionary<Weaponry, UnitHealth> activeWeapons = new Dictionary<Weaponry, UnitHealth>();
 
     private void Awake()
     {
@@ -25,15 +28,67 @@ public class BattleManager : MonoBehaviour
     public void StartAttack(Weaponry attacker, UnitHealth target)
     {
         OnAttackStarted?.Invoke(attacker, target);
+        RegisterAttack(attacker, target);
     }
 
     public void StopAttack(Weaponry attacker, UnitHealth target)
     {
         OnAttackStopped?.Invoke(attacker, target);
+        UnregisterAttack(attacker);
     }
 
     public void NotifyDeath(UnitHealth target)
     {
         OnUnitDied?.Invoke(target);
+        UnregisterByKill(target);
+    }
+    
+    /// <summary>
+    /// Registers an attacker 'Weaponry' and/or updates his target 'UnitHealth'
+    /// </summary>
+    /// <param name="attacker"></param>
+    /// <param name="target"></param>
+    private void RegisterAttack(Weaponry attacker, UnitHealth target)
+    {
+        if ( ! activeWeapons.TryGetValue(attacker, out var targetsHealth))
+            activeWeapons.Add(attacker, target);
+        else
+            activeWeapons[attacker] = target;
+    }
+
+    /// <summary>
+    /// Removes or Unregisters an attacker 'Weaponry'
+    /// </summary>
+    /// <param name="attacker"></param>
+    private void UnregisterAttack(Weaponry attacker)
+    {
+        if (activeWeapons.TryGetValue(attacker, out var targetsHealth))
+            activeWeapons.Remove(attacker);
+    }
+
+    /// <summary>
+    /// Removes each attacker 'Weaponry' when a target 'UnitHealth' got killed
+    /// </summary>
+    /// <param name="killedTarget"></param>
+    private void UnregisterByKill(UnitHealth killedTarget)
+    {
+        // This List will store the attackers 'Weaponry' who will be removed from activeWeapons 'Dictionary'
+        var keysToRemove = new List<Weaponry>();
+        
+        // Returns if activeWeapons does not even have the killedTarget registered under an attacker
+        // Adds every attacker/'Weaponry'/key-from-activeWeapons to keysToRemove<List>
+        if (activeWeapons.ContainsValue(killedTarget))
+        {
+            foreach (var activeWeapon in activeWeapons.Keys)
+                if (activeWeapons.TryGetValue(activeWeapon, out var target) == killedTarget)
+                    keysToRemove.Add(activeWeapon);
+        }
+        else return;
+        
+        // Removes all killing attackers
+        foreach (var weaponry in keysToRemove)
+            activeWeapons.Remove(weaponry);
+        
+        keysToRemove.Clear();
     }
 }
